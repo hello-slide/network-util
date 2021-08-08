d2ee0ddfabff520f857d7e552e03b1de48b663d2
Yuto Watanabe <yuto.w51942@gmail.com>
2021-08-08 16:12:16 +0900

    added status func

--- status.go
+++ status.go
@@ -1,7 +1,28 @@
 package networkutil
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type errorResponse struct {
+	StatusCode int    `json:"status_code"`
+	Status     string `json:"status"`
+}
 
 func ErrorStatus(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusBadRequest)
+}
+
+func ErrorResponse(w http.ResponseWriter, statusCode int, err error) {
+	response := errorResponse{
+		StatusCode: statusCode,
+		Status:     err.Error(),
+	}
+	responseText, err := json.Marshal(response)
+	if err != nil {
+		ErrorResponse(w, 1, err)
+	}
+
+	w.Write(responseText)
 }
