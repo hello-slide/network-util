1ac428d902f9aff3a1d26eb4459f0bc847e1c194
Yuto Watanabe <yuto.w51942@gmail.com>
2021-08-28 18:41:06 +0900

    added cors urls

--- corsConfig.go
+++ corsConfig.go
@@ -5,6 +5,8 @@ import "github.com/rs/cors"
 var CorsConfig = cors.New(cors.Options{
 	AllowedOrigins: []string{
 		"https://hello-slide.jp",
+		"https://hello-slide.com",
+		"https://hello-slide.net",
 		"http://localhost:3000",
 	},
 })
